dc70ce8d011e67856c0176ba1ac3b3aa97c8e3f6
Vess Bakalov <vess@pliant.io>
2016-12-22 16:26:34 -0500

    Its ALIVE!!!

--- DupeDiscover/src/main/java/com/codr1/dupediscover/DupeDiscoverUI.java
+++ DupeDiscover/src/main/java/com/codr1/dupediscover/DupeDiscoverUI.java
@@ -12,14 +12,18 @@ import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.data.Item;
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Table;
 import com.vaadin.data.util.FilesystemContainer;
+import com.vaadin.data.util.TextFileProperty;
+import com.vaadin.shared.ui.label.ContentMode;
 import java.io.File;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.GridLayout;
-import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TreeTable;
 import com.vaadin.ui.VerticalLayout;
@@ -42,6 +46,7 @@ import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import org.vaadin.dialogs.ConfirmDialog;
 
 /**
  *
@@ -49,12 +54,58 @@ import java.util.Set;
 @Push
 @Theme("mytheme")
 @Widgetset("com.codr1.dupediscover.MyAppWidgetset")
+
 public class DupeDiscoverUI extends UI {
     static long currentTime = System.currentTimeMillis() / 1000;
     static long oldTime = System.currentTimeMillis() / 1000; 
     static Integer  numberFilesScanned = 0;
     
     Multimap<String,File> allFiles; 
+    
+    VerticalSplitPanel vSplitLeft;
+    VerticalSplitPanel vSplitRight;
+    GridLayout topRightGrid;
+    VerticalLayout topLeftVertical;
+    VerticalLayout bottomLeftVertical;
+    VerticalLayout bottomRightVertical;
+
+    HorizontalSplitPanel hSplit;
+    
+    void showInitialDialog() {
+        String confirmMsg = 
+                "WARNING!  IMPROPER USE OF THIS PROGRAM COULD DAMAGE YOUR COMPUTER SYSTEM!!!<br>" +
+                "<b>PROCEED AT YOUR OWN RISK!  IF YOU ARE NOT COMFORTABLE PLEASE QUIT RIGHT AWAY!</b></br><br>" +
+                "This program is meant to help with photo and other file organization.  "+
+                "Even as I tried to have an organized repository of photos, I had many "+
+                "camera dumps and phone backups living on my systems.  Cleaning those out "+
+                "every several months was difficult and very very time consuming.<br>"+
+                "This program helps you identify duplicates quickly and choose the master copy in bulk.<br> "+
+                "<ol><li> Select directories to scan.  The list of selected directories appars in the top right pane. "+
+                "<li> Click the 'Scan' button.  The program will scan recursivelly and identify all duplicates " +
+                "with the exact name and size match.  They will appear in the bottom left window."+
+                "<li> Select the 'Master' copy of a file.</ol><br>" +
+                "<b><i> At this time the program will walk throught the directory of the 'master' "+
+                "as well as those of the non-master.  It will delete any file in the non-master directories "+
+                "that is duplicated (has the exact same name and size) in the master directory. " +
+                "Files that are not duplicated in teh master will not be touched.<br>" +
+                
+                "The original in the master will always remain (the program will never delete all copies of a file).<br>"+
+                "</i></b><br>"+
+                "<b> AT THIS TIMES DUPLICATE FILES WILL BE IREVOCABLY DELETED!  EXCERCISE CAUTION!</b>"+
+                "";
+               
+        ConfirmDialog.show(this, confirmMsg, new ConfirmDialog.Listener() {
+            @Override
+            public void onClose(ConfirmDialog cd) {
+                if( cd.isConfirmed() ){
+                    //Nothing happens 
+                   
+                } else {
+                    System.exit(0);
+                }
+            }
+        }).setContentMode(ConfirmDialog.ContentMode.HTML);
+    }
     
     FilenameFilter fileFilter = new FilenameFilter() {
         @Override
@@ -87,7 +138,7 @@ public class DupeDiscoverUI extends UI {
         if( file.isDirectory() ) {
             return;
         }
-        allFiles.put( file.getName(), file );
+        allFiles.put( file.getName() + "/" + file.length() , file );
     }
    
     public /*static*/ class Finder 
@@ -112,10 +163,8 @@ public class DupeDiscoverUI extends UI {
             if( Thread.currentThread().isInterrupted() ){
                 return FileVisitResult.TERMINATE;
             }
-            return CONTINUE;
-            
+            return CONTINUE;   
         }
-        
     }
     
     class ScanThread extends Thread {
@@ -132,6 +181,15 @@ public class DupeDiscoverUI extends UI {
                     }
                 });
                 return;
+            } else {
+                access( new Runnable() {
+                    @Override
+                    public void run() {
+                        bottomLeftVertical.removeComponent(foundDuplicates);
+                        buildFoundDuplicatesTable();
+                        bottomLeftVertical.addComponent(foundDuplicates);   
+                    }
+                });
             }
             numberFilesScanned = 0;
             
@@ -152,18 +210,50 @@ public class DupeDiscoverUI extends UI {
                 }
             }
             
+            access( new Runnable() {
+                @Override
+                public void run() {
+                    currentFileBox.setValue( "Parsing Results..." );
+           
+                    Set keySet = allFiles.keySet();
+                    Iterator keyIter = keySet.iterator();
+                    while( keyIter.hasNext() ){
+                        String key = (String) keyIter.next();
+                        Collection<File> values = allFiles.get( key );
+                        
+                        if( values.size() > 1 ) {
+                            Object newParent = null;
+                            Object newRow;
+                            for( File currentFile : values ) {
+                                String currentFileName = currentFile.getName();
+                        
+                                // Handle the first row (the parent row)
+                                if( newParent == null ) {
+                                    newParent = foundDuplicates.addItem( new Object[]{currentFileName, null, null }, null );
+                                    foundDuplicates.setCollapsed(newParent, false);
+                                }
+                                
+                                newRow = foundDuplicates.addItem( new Object[]{currentFileName, currentFile.getParent(), currentFile.length() }, 
+                                        currentFile.getAbsolutePath() );
+                                foundDuplicates.setParent( newRow, newParent );                  
+                            }  
+                        }
+                    }   
+                }    
+            });
+            
             // When the scan is finished, lets update the UI. 
             access( new Runnable() {
-                    @Override
-                    public void run() {
-                        Notification.show( "Scan Completed!" );
+                @Override
+                public void run() {
+                    Notification.show( "Scan Completed!" );
 
-                        startScan.setEnabled(true);
-                        cancelScan.setEnabled(false);
-                        currentFileBox.setValue("Scan Completed");
-                        numberFilesBox.setValue( numberFilesScanned.toString() );
-                    }
-                });
+                    startScan.setEnabled(true);
+                    cancelScan.setEnabled(false);
+                    currentFileBox.setValue("Scan Completed");
+                    numberFilesBox.setValue( numberFilesScanned.toString() );
+                }
+            });
         }
     }
             
@@ -176,6 +266,8 @@ public class DupeDiscoverUI extends UI {
     final TextField currentFileBox = new TextField();
     final TextField numberFilesBox = new TextField();
     
+    Label preview = new Label( "", ContentMode.HTML );
+    
     Thread scanThread;
     
     private void startScan() {
@@ -184,22 +276,159 @@ public class DupeDiscoverUI extends UI {
         scanThread.start();
     }
     
+    private void buildFoundDuplicatesTable() {
+        foundDuplicates = new TreeTable( "Found Duplicates" );
+        foundDuplicates.setHeight( "100%" );
+        foundDuplicates.setWidth( "100%" );
+        
+        foundDuplicates.addContainerProperty("File Name", String.class, null);
+        foundDuplicates.addContainerProperty("Directory", String.class, null);
+        foundDuplicates.addContainerProperty("Size", Long.class, null);
+        
+        foundDuplicates.addValueChangeListener(new ValueChangeListener() {
+            @Override
+            public void valueChange(Property.ValueChangeEvent event) {
+                Integer rowNumber = (Integer)event.getProperty().getValue(); 
+                Item selectedRow = foundDuplicates.getItem( rowNumber );
+                if( selectedRow.getItemProperty("Directory").getValue() == null ){
+                    Notification.show( "Please select an entry with a file and directory" );
+                    return;
+                }
+                
+                String fileName = selectedRow.getItemProperty("Directory").getValue().toString()+ File.separator +selectedRow.getItemProperty("File Name").getValue().toString();
+                
+                //Hack to handle images
+                if( fileName.toLowerCase().endsWith("png") ||  
+                        fileName.toLowerCase().endsWith("jpg") || 
+                        fileName.toLowerCase().endsWith("jpeg") || 
+                        fileName.toLowerCase().endsWith("gif")
+                ) {
+                    if( preview.isAttached() ){
+                        bottomRightVertical.removeComponent( preview );
+                    }
+                    preview = new Label( "<img src=\"" + "file:////" + new File(fileName).getAbsolutePath() + "\"/> ", ContentMode.HTML );
+                    bottomRightVertical.addComponent(preview);
+                } else { 
+                    if( preview.isAttached() ){
+                        bottomRightVertical.removeComponent( preview );
+                    }
+                    preview = new Label( "", ContentMode.HTML );
+                    preview.setPropertyDataSource( new TextFileProperty(new File( fileName ) ) );
+                    bottomRightVertical.addComponent(preview);
+                }
+            }
+        });
+        
+         foundDuplicates.addGeneratedColumn("Set as Master Copy", new TreeTable.ColumnGenerator() {
+            @Override
+            public Object generateCell(Table source, final Object itemId, Object columnId) {
+                Item currentRow = source.getItem(itemId);
+                if( currentRow.getItemProperty("Directory").getValue() == null ) {
+                    return "";
+                }
+                
+                Button buttonSetMaster = new Button( "Set Master" );
+                buttonSetMaster.addClickListener( new Button.ClickListener() {
+                    @Override
+                    public void buttonClick(Button.ClickEvent event) {               
+                        cleanUpFiles( itemId );
+                    }
+                });
+                return buttonSetMaster;
+            }
+        });
+        foundDuplicates.setColumnExpandRatio( "Directory", 1 );
+        
+        foundDuplicates.setImmediate(true);
+        foundDuplicates.setSelectable(true);
+    }
+    
+    private void cleanUpFiles( Object itemId ) {
+        
+        Item masterRow = foundDuplicates.getItem(itemId);
+        Object parentRowId = foundDuplicates.getParent(itemId);
+        Item parentRow = foundDuplicates.getItem(itemId);
+        String fileName = masterRow.getItemProperty("File Name").getValue().toString();
+        String masterDirName =  masterRow.getItemProperty("Directory").getValue().toString();
+        final ArrayList<String> finalList = new ArrayList();
+      
+        
+        Collection cleanUpTargets =  new ArrayList(foundDuplicates.getChildren(parentRowId) );
+        //remove the 'Master'
+        cleanUpTargets.remove(itemId);
+        
+        // Create an array of all the dirs to be cleaned up
+        String[] dirsToClean = new String[cleanUpTargets.size()];
+        int i = 0;
+        for( Iterator iterator = cleanUpTargets.iterator(); iterator.hasNext(); i++ ){
+            Item currentRow = (Item) foundDuplicates.getItem( iterator.next() );
+            dirsToClean[i] = currentRow.getItemProperty("Directory").getValue().toString();
+        }
+        
+        // Go through all the files in the master directory
+        File masterDirectory = new File( masterDirName );
+        File[] masterFileList = masterDirectory.listFiles();
+        if( masterFileList != null ){
+            for( File currentFile : masterFileList ){
+                // do not deal with directories
+                if( currentFile.isDirectory() ){
+                    continue;
+                }
+                
+                // check if this file name exists in any of the other dirs to clean
+                for( String dirToClean : dirsToClean ){
+                    File fileToClean = new File( dirToClean + File.separator + currentFile.getName() );
+                    if( fileToClean.exists() ){
+                        finalList.add(fileToClean.getAbsolutePath());
+                    }
+                }
+            }
+            
+            //Create a fonfirm message
+            String confirmMsg = "The following files will be deleted:\n";
+            for( String fileToDelete : finalList ){
+                confirmMsg = confirmMsg + fileToDelete + "\n";
+            }
+            
+            
+            ConfirmDialog.show(this, confirmMsg, new ConfirmDialog.Listener() {
+                @Override
+                public void onClose(ConfirmDialog cd) {
+                    if( cd.isConfirmed() ){
+                        // Delete the files and the entries in the table...
+                        for( String fileToDelete : finalList ){
+                            if( new File( fileToDelete ).delete() ) {
+                                foundDuplicates.removeItem(fileToDelete);
+                            } else {
+                                Notification.show( fileToDelete + " did not get deleted!  Aborting!" );
+                                break;
+                            }
+                        }
+                    } else {
+                        Notification.show( "Doom Averted" );        
+                    }
+                }
+            });
+        }
+    }
     
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        VerticalSplitPanel vSplitLeft = new VerticalSplitPanel();
-        VerticalSplitPanel vSplitRight = new VerticalSplitPanel();
-        GridLayout topRightGrid = new GridLayout( 5, 2 );
-        VerticalLayout topLeftVertical = new VerticalLayout();
-        VerticalLayout bottomLeftVertical = new VerticalLayout();
+        vSplitLeft = new VerticalSplitPanel();
+        vSplitRight = new VerticalSplitPanel();
+        topRightGrid = new GridLayout( 5, 2 );
+        topLeftVertical = new VerticalLayout();
+        bottomLeftVertical = new VerticalLayout();
+        bottomRightVertical = new VerticalLayout();
         
-        HorizontalSplitPanel hSplit = new HorizontalSplitPanel(vSplitLeft, vSplitRight);
+        hSplit = new HorizontalSplitPanel(vSplitLeft, vSplitRight);
         
         selectedDirectories.addContainerProperty( "Name", String.class, null );
         
         vSplitLeft.addComponent( topLeftVertical );
         vSplitRight.addComponent( topRightGrid );
         vSplitLeft.addComponent( bottomLeftVertical );
+        vSplitRight.addComponent( bottomRightVertical );
         
         topLeftVertical.addComponent(availableDirectories);
         
@@ -225,20 +454,16 @@ public class DupeDiscoverUI extends UI {
         availableDirectories.setWidth("100%");
         availableDirectories.setHeight("100%");
         
-        
-        
+        buildFoundDuplicatesTable();
         bottomLeftVertical.addComponent( foundDuplicates );
-        foundDuplicates.setHeight( "100%" );
-        foundDuplicates.setWidth( "100%" );
-        
-        foundDuplicates.addContainerProperty("File Name", String.class, null);
-        foundDuplicates.addContainerProperty("Directory", String.class, null);
-        foundDuplicates.addContainerProperty("Size", Integer.class, null);
-        
+        bottomRightVertical.addComponent( preview );
+       
         setContent( hSplit );
         
         final Notification notify = new Notification("hi");
         notify.setDelayMsec(20000);
+        
+        showInitialDialog();
         
         // Add a column with the add button to availableDirectories
         availableDirectories.addGeneratedColumn("Add", new TreeTable.ColumnGenerator() {
@@ -278,8 +503,8 @@ public class DupeDiscoverUI extends UI {
                             }
                             
                             // Check if the new directory is a sub directory of the existing directory
-                            ////NOTE: We cant just check names since /hi and /hi22 will have a problem
-                            ////So we check with the parent of the current dierctory
+                            ////NOTE: We cant just check names since /hi and /hi22 will have a problem 
+                           ////So we check with the parent of the current dierctory
                             String parentPath = itemToAddName.substring(0, itemToAddName.lastIndexOf(File.separator));
                             if( parentPath.length() > 0 && currentDirectory.contains( parentPath )) {
                                 notify.show("Not adding " + itemToAdd.toString() + " since its parent is already selected." );
@@ -318,7 +543,7 @@ public class DupeDiscoverUI extends UI {
                 return buttonRemove;
             }
         });
-        
+       
         startScan.addClickListener( new Button.ClickListener() {
             @Override
             public void buttonClick(Button.ClickEvent event) {
@@ -332,31 +557,11 @@ public class DupeDiscoverUI extends UI {
                 } else {
                     startScan();
                     notify.show("Scan started!");
-                }
-                                
-                try{
-                    scanThread.join();
-                } catch (InterruptedException e ){
-                    notify.show( "Getting interrupted up in here!" );
-                }
-                
-                currentFileBox.setValue( "Looking for duplicates..." );
-                
-                Set keySet = allFiles.keySet();
-                Iterator keyIter = keySet.iterator();
-                while( keyIter.hasNext() ){
-                    String key = (String) keyIter.next();
-                    Collection<File> values = allFiles.get( key );
-                    if( values.size() > 1 ) {
-                        for( File currentFile : values ) {
-                            foundDuplicates.addItem( new Object[]{currentFile.toString(), null, currentFile.length() } );
-                        }  
-                    }
-                    
-                    
-                }
+                }     
             }
         });
+        
+        
         
         
         cancelScan.addClickListener( new Button.ClickListener() {
